91e2f5b96e346e08efd19dfd411c10d20798d7de
baddepudisaideepak <baddepudisaideepak@gmail.com>
2024-01-18 15:29:30 +0000

    kmean added

--- clusteringandfitting.py
+++ clusteringandfitting.py
@@ -100,3 +100,36 @@ df_norm = scaler.transform(df_clust)
 for ic in range(2, 11):
     score = one_silhoutte(df_norm, ic)
     print(f"The silhouette score for {ic: 3d} is {score: 7.4f}")   # allow for minus signs
+
+# set up the clusterer with the number of expected clusters
+kmeans = cluster.KMeans(n_clusters=2, n_init=20)
+
+# Fit the data, results are stored in the kmeans object
+kmeans.fit(df_norm)     # fit done on x,y pairs
+
+# extract cluster labels
+labels = kmeans.labels_
+
+# extract the estimated cluster centres and convert to original scales
+cen = kmeans.cluster_centers_
+cen = scaler.inverse_transform(cen)
+xkmeans = cen[:, 0]
+ykmeans = cen[:, 1]
+
+# extract x and y values of data points
+x = world["Mortality"]
+y = world["Poverty"]
+
+plt.figure(figsize=(8.0, 8.0))
+
+# plot data with kmeans cluster number
+plt.scatter(x, y, 10, labels, marker="o", cmap=cm)
+
+# show cluster centres
+plt.scatter(xkmeans, ykmeans, 45, "k", marker="d")
+
+plt.xlabel("Mortality")
+plt.ylabel("Poverty")
+plt.title("Mortality vs Poverty")
+plt.savefig("Mortality_Povery.png", dpi =300)
+plt.show()
